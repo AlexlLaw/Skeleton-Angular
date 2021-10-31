47cb203b3c2443c17e910ed2b43da6ae11d0dff5
Álex Law <junior-302@hotmail.com>
2021-10-31 00:29:44 -0300

    Adicionando biblioteca fontAwesome e icons no menu

--- Project-Angular/src/app/app.module.ts
+++ Project-Angular/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     CommonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- Project-Angular/src/app/core/components/menu/menu.module.ts
+++ Project-Angular/src/app/core/components/menu/menu.module.ts
@@ -2,12 +2,14 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { MenuComponent } from './menu.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 @NgModule({
   declarations: [MenuComponent],
   imports: [
     CommonModule,
+    FontAwesomeModule
 
   ], exports: [
     MenuComponent
